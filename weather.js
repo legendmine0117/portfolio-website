1ba8da85cd32f41ce6b49f4454a66be00c485e27
Xin (Alex) Zhong <alexzhong1031@gmail.com>
2021-04-17 18:46:30 -0400

    rearrange

--- weather.js
+++ weather.js
@@ -1,5 +1,6 @@
 const API_KEY = config_key.KEY_1
 
+
 const openAq_url = `https://api.airvisual.com/v2/city?city=New York City&state=New York&country=USA&key=${API_KEY}`
     async function getWeather() {
         const response = await fetch(openAq_url)
