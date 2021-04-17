c5664a534b01a0d32397d5e22d66ae515d73e2d7
Xin (Alex) Zhong <alexzhong1031@gmail.com>
2021-04-17 17:40:09 -0400

    weather app

---
+++ Weather/app.js
@@ -0,0 +1,14 @@
+const API_KEY = config.KEY_1
+
+const openAq_url = `https://api.airvisual.com/v2/city?city=New York City&state=New York&country=USA&key=${API_KEY}`
+    async function getWeather() {
+        const response = await fetch(openAq_url)
+        const responseData = await response.json()
+        last_updated_time.innerHTML = new Date(responseData.data.current.weather.ts);
+        nyc_temperature.innerHTML = responseData.data.current.weather.tp
+        nyc_AQI.innerHTML = responseData.data.current.pollution.aqius
+        nyc_humidity.innerHTML = responseData.data.current.weather.hu
+}
+getWeather()
+
+
